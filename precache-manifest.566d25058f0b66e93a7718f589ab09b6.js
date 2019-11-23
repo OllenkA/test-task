6c9ab5cae2771c368f2295c54f6eb2c7fdd4ae5a
OllenkA <shamko.olga2018@gmail.com>
2019-11-23 14:32:19 +0300

    Updates

---
+++ precache-manifest.566d25058f0b66e93a7718f589ab09b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c78022e7b37a57fec9182046068203",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "7b631d0e652404c23de5",
+    "url": "/profile-page/static/css/main.817bd4dd.chunk.css"
+  },
+  {
+    "revision": "7e975c776313057e7967",
+    "url": "/profile-page/static/js/2.bbc6bb25.chunk.js"
+  },
+  {
+    "revision": "7b631d0e652404c23de5",
+    "url": "/profile-page/static/js/main.20ddfc8d.chunk.js"
+  },
+  {
+    "revision": "96f962f7fcb4375d5e88",
+    "url": "/profile-page/static/js/runtime-main.519ee1af.js"
+  },
+  {
+    "revision": "6e0baffe1be646cb36d9b82d23b3dc01",
+    "url": "/profile-page/static/media/photo_2019-11-11_20-16-51.6e0baffe.jpg"
+  }
+]);
